de174a25af3b44b0444dc892c7a78066e173c68f
Pat Rocchio <patrocc6@gmail.com>
2017-05-17 04:44:51 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :items, :transactions
+
+  def initialize(employee=nil)
+    @total = 0
+    @discount = 20 if employee
+    @items = []
+    @transactions = []
+  end
+
+  def add_item(title, price, quantity=1)
+    @total += price * quantity
+    quantity.times { @items << title }
+    @transactions << price * quantity
+  end
+
+  def apply_discount
+    if @discount
+      @total -= @total * (@discount/100.00)
+      "After the discount, the total comes to $#{@total.round}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    @total -= @transactions.pop
+  end
+end
